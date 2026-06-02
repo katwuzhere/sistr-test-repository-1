4e91dd2e7d3c220d99d7d4200a3053dcb731a267
katWuzHere <kvc750@gmail.com>
2026-06-02 10:16:05 -0500

    remove supabase lines

--- src/auth/supabase.js
+++ src/auth/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://mysuperspecialsupabaseprojectforsistrtestrepo1.supabase.co";
-const supabase_db_password = "ohno"
-const supabase_secret_api_key = "ohno"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
