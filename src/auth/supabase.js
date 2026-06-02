7d696cdce89e443b0ffc5d195a63219866f0f13b
katWuzHere <kvc750@gmail.com>
2026-06-02 10:15:39 -0500

    supabase clean commit

--- src/auth/supabase.js
+++ src/auth/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://mysuperspecialsupabaseprojectforsistrtestrepo1.supabase.co";
-const supabase_db_password = "D1iPlLQWNP5sVDCl"
-const supabase_secret_api_key = "sb_secret_j6POHlaAugvJSV-J2CZ3Kw_1nMIIJhZ"
+const supabase_db_password = "ohno"
+const supabase_secret_api_key = "ohno"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
