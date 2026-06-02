c292d2c3aee6556f459ada07ab828fe805bf4170
katWuzHere <kvc750@gmail.com>
2026-06-02 10:31:59 -0500

    add gitlab pat for tag

---
+++ src/apis/gitlab.js
@@ -0,0 +1,4 @@
+export const gitlab_pat_legacy = "glpat-m3C-gr30D8xKmvkxncOFqmM6MQpvOjEKdTpuMzRpMw8.01.171l7v9ry";
+
+export const gitlab_fine_grained_pat_beta = "glpat-yYLNfiRELZFllSXgmsH5RGM6MQpvOjEKdTpuMzRpMw8.01.171xtla69";
+
